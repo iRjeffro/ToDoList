fd042df46f22b2c6541de896ea24ef3d4c15471b
iRjeffro <jg.jeffgordon4@gmail.com>
2023-07-12 20:51:31 -0600

    fixed bugs causing button to delete before being able to press, and creating multiple buttons

--- script.js
+++ script.js
@@ -2,58 +2,59 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-
 function inputLength() {
-	return input.value.length;
+  return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+  if (inputLength() > 0) {
+    createListElement();
+  }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
 }
 
 function addToDoneList(event) {
-	if (event.target.tagName.toLowerCase() === "li") {
-		event.target.classList.toggle("done");
-	}
+  if (event.target.tagName.toLowerCase() === "li") {
+    event.target.classList.toggle("done");
+  }
 }
 
 function addDeleteButton(event) {
-	if (event.target.tagName.toLowerCase() === "li") {
-	  var trashBtnAdd = document.createElement("button");
-	  trashBtnAdd.appendChild(document.createTextNode("X"));
-	  event.target.appendChild(trashBtnAdd);
-	}
+  var listItem = event.target;
+  var deleteBtn = listItem.querySelector("button");
+  if (listItem.tagName.toLowerCase() === "li" && !deleteBtn) {
+    deleteBtn = document.createElement("button");
+    deleteBtn.appendChild(document.createTextNode("X"));
+    listItem.appendChild(deleteBtn);
+  }
 }
 
 function removeDeleteButton(event) {
-	if (event.target.tagName.toLowerCase() === "li") {
-	  var deleteBtn = event.target.querySelector("button");
-	  if (deleteBtn) {
-		event.target.removeChild(deleteBtn);
-	  }
-	}
+  var listItem = event.target;
+  var deleteBtn = listItem.querySelector("button");
+  if (listItem.tagName.toLowerCase() === "li" && deleteBtn && !listItem.contains(event.relatedTarget)) {
+    listItem.removeChild(deleteBtn);
+  }
 }
 
 function deleteItem(event) {
-	if (event.target.tagName.toLowerCase() === "button") {
-	  var listItem = event.target.parentNode;
-	  listItem.parentNode.removeChild(listItem);
-	}
+  if (event.target.tagName.toLowerCase() === "button") {
+    var deleteBtn = event.target;
+    var listItem = deleteBtn.parentNode;
+    listItem.parentNode.removeChild(listItem);
+  }
 }
 
 button.addEventListener("click", addListAfterClick);
